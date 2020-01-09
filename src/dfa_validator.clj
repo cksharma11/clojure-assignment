e196a9345e078426734c3b05536fa3dad342cfc4
Chandan Kumar <cksharma122@gmail.com>
2020-01-09 23:40:30 +0530

    Add reduce to dfa-validator

--- src/dfa_validator.clj
+++ src/dfa_validator.clj
@@ -10,10 +10,15 @@
                         :b {:0 :b, :1 :b},
                         :c {:0 :c, :1 :c}}})
 
-(defn is-valid-dfa [input dfa]
+(defn is-valid-dfa-1 [input dfa]
   (loop [current (:start dfa) i input]
     (if (empty? i)
       (contains? (:accepts dfa) current)
       (recur ((first i) (current (:transitions dfa))) (rest i)))))
 
-(println (is-valid-dfa '(:0 :1 :1 :1) dfa))
+(defn is-valid-dfa-2 [candidate dfa]
+  (contains? (:accepts dfa)
+             (reduce #(%2 (%1 (:transitions dfa))) (:start dfa) candidate)))
+
+(println (is-valid-dfa-1 '(:0 :1 :0 :1) dfa))
+(println (is-valid-dfa-2 '(:0 :1 :0 :1) dfa))
